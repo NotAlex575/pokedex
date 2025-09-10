6b14e04339f199d6bbc1d562440cd9f278bfe683
NotAlex575 <dapontealessandro89gmail.com>
2025-09-10 12:52:23 +0200

    inizializzati gli import necessari per il routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Homepage from "./pages/Homepage"
+import About from "./pages/About"
+import Pokedex from "./pages/Pokedex"
+import PokemonSingle from "./pages/PokemonSingle"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom" //SERVIZIO ROUTING
+
 function App() {
   return (
     <></>
