5689da6626331f959dca266833d89c1e7c2d9216
NotAlex575 <dapontealessandro89gmail.com>
2025-09-10 13:53:28 +0200

    aggiornato lato grafico del pokedex (tramite anche un piccolo uso di css in index.css)

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -9,18 +9,34 @@ const Pokedex = () => {
       .then(data => setPokemon(data))
   }, []);
 
-  if(!pokemon) return <div>Loading....</div>;
+  if (!pokemon) return <div>Loading....</div>;
 
   return (
     <>
-      <div className="container text-center">
-        <div className="col-12">
-          <img className="img-fluid" src={pokemon.sprites.front_default} alt={pokemon.name} />
-          <h2>{pokemon.name}</h2>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="card border-danger shadow-lg w-35">
+          <img
+            className="card-img-top p-3 img-fluid mx-auto d-block"
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+          <div className="card-body text-center">
+            <h2 className="card-title text-capitalize fs-2">{pokemon.name}</h2>
+            <div>
+              {pokemon.types.map(({ type }) => (
+                <span
+                  key={type.name}
+                  className="badge bg-danger text-capitalize mx-2 fs-5"
+                >
+                  {type.name}
+                </span>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Pokedex;
