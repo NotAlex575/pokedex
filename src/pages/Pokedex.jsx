f08ad30ec3086a530f66ba3d897bf2b3c04f35ee
NotAlex575 <dapontealessandro89gmail.com>
2025-09-10 13:47:06 +0200

    updatato Pokedex.jsx, che mostra il 1 pokemon del pokedex

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 const Pokedex = () => {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then(res => res.json())
+      .then(data => setPokemon(data))
+  }, []);
+
+  if(!pokemon) return <div>Loading....</div>;
+
   return (
-    <div className="text-center fs-1 fw.bold mb-5">Pokedex page</div>
+    <>
+      <div className="container text-center">
+        <div className="col-12">
+          <img className="img-fluid" src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <h2>{pokemon.name}</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Pokedex
+export default Pokedex;
